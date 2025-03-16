2b06bdc213746479b67971aff9cff51faf1d78d4
Abdoul Aziz Abdoulaye <asouabdoulaye@gmail.com>
2025-03-16 23:42:02 +0000

    init repo

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Electronic signature',
+    'version' : '1.0',
+    'author': 'Moore sn',
+    'maintainer': 'Moore sn',
+    'summary': 'Electronic signature',
+    'sequence': 1,
+    'description': """
+SMS marketing 
+====================
+This module allow you sign your document
+    """,
+    'category': 'purchase',
+    'website': 'https://moore.sn',
+    'images' : ['images/icon.png'],
+    'depends' : ['base_setup', 'purchase'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'autoinstall': False,
+}
